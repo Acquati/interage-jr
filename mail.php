048d0f381b1119df1094aebf2ed65a263d3f2353
Leandro Acquati <leandroacquati@gmail.com>
2017-10-18 12:51:05 -0200

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,22 @@
+﻿<?php
+
+$nome = $_POST['nome'];
+$email= $_POST['email'];
+$telefone= $_POST['nome'];
+$assunto= $_POST['assunto'];
+$mensagem= $_POST['mensagem'];
+$corpo ="<strong>Nome: </strong>".$nome."<br><br><strong>Email: </strong>".$email."<br><br><strong>Telefone: </strong>".$telefone."<br><br><strong>Mensagem: <br><br></strong>".$mensagem."";
+
+
+$headers =  "Content-Type:text/html; charset=UTF-8\n";
+  $headers .= "From:  interagejr.com.br\n";
+  $headers .= "X-Sender:  \n";
+  $headers .= "X-Mailer: PHP  v".phpversion()."\n";
+  $headers .= "X-IP:  ".$_SERVER['REMOTE_ADDR']."\n";
+  $headers .= "Return-Path:  \n";
+  $headers .= "MIME-Version: 1.0\n";
+
+mail('einteragejr@gmail.com', $assunto, $corpo, $headers);  //função que faz o envio do email.
+
+
+?>
